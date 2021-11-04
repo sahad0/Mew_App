924a08a0107b6261c39a8cb4be415a9dac540dde
Sahad <sahadwg@gmail.com>
2021-11-05 02:13:20 +0530

    added some comments for clarity

--- client/pages/newsfeed.js
+++ client/pages/newsfeed.js
@@ -9,16 +9,23 @@ import Suggestions from "../Components/Suggestions/suggestion";
 
 
 function Newsfeed() {
-
-    //fetch states and set card
+    //Context
     const[state,setstate] = useContext(UserContext)['state1'];
-    const [content,setcontent] = useState("");          //contents
+
+    
+    //post contents-- Text editor
+    const [content,setcontent] = useState("");   
+    //post images   -- Text Editor  
     const [image,setImage] = useState({
         url:"",
         public_id: "",
     });
+    //---Image loading symbol state
     const [loading,setLoading] = useState(false);
 
+
+
+    //posts array state
     const [cards,setCards] = useState([]);
 
 
@@ -27,39 +34,42 @@ function Newsfeed() {
     const [deletedid,setdeletedid] = useState("");
     const [okdelete,setokaydelete] = useState(false);
 
+
     //Suggestion State
     const[people,setpeople] = useState([]);
 
 
     
 
-
-
+    //fixing  button on rich editor empty content
     useEffect(()=>{
-        if(content === "<p><br></p>"){              //disable button on rich editor empty content
+        if(content === "<p><br></p>"){              
             setcontent("")
         }
     },[content]);
     
 
      useEffect(()=>{
-       userPost();                  //fetch on mount
-       findSuggestions();           //friend Suggestion
+        //fetch posts on mount
+        userPost();  
+        //friend Suggestion
+        findSuggestions();           
      },[state && state.token]);
 
-     useEffect(()=>{
-        window.localStorage.getItem("update") && window.localStorage.removeItem("update"); //for update removal 
+    //on return from update page removes update key from local storage (toggling post keys)
+    useEffect(()=>{
+        window.localStorage.getItem("update") && window.localStorage.removeItem("update"); 
     },[]);
 
 
 
-    //delete useeffects
+    //Modal appearing before delete
     useEffect(()=>{
         if(deletedid.length>0){
             setIsDeleteModalVisible(true);
         }
     },[deletedid]);
-
+    //takes effect when set okay on modal
     useEffect(()=>{
         if(okdelete === true){
             deletethepost();
@@ -70,9 +80,11 @@ function Newsfeed() {
     },[okdelete]);
 
 
+
+
 //Part One Functions
 
-//fetch post  and create post functions
+//Making a post in text editor
     async function contentextract(e){
         e.preventDefault();
         try{
@@ -103,6 +115,9 @@ function Newsfeed() {
             return toast.error("Internal Server Error");
         }
     }
+
+
+    //handling image and getting url and setting up in state
     async function handleImage(e){
         const files = e.target.files[0];
         let formData = new FormData();
@@ -122,7 +137,7 @@ function Newsfeed() {
         }
     }
 
-
+    //refetching posts for certain constions like update and delete
     async function userPost(){
         try{
             const hello = await axios.get("/followerspost");     //refetch posts especialy for after delete and update !important
