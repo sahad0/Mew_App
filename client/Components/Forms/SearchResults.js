a1d811b219e05f6cc510145ab19984d4919e8184
Sahad <sahadwg@gmail.com>
2021-12-30 01:05:01 +0530

    search form update

--- client/Components/Forms/SearchResults.js
+++ client/Components/Forms/SearchResults.js
@@ -7,24 +7,27 @@ function SearchResults({showSearch,results,haveImage,checkStatus}) {
 
         return (
             <>
-            <div className="col-md-4" >
-                <List className="sm"
-                    itemLayout="horizontal"
-                    dataSource={results}
-                    renderItem={(user) => (
-                    <List.Item>
-                        <List.Item.Meta
-                        avatar={<Avatar shape="square" size="large" src={haveImage(user)} />}
-                        title={<div className="d-flex justify-content-between py-2">
-                                <div style={{fontStyle:"italic"}}>{user.name}</div>
-                                <Button type="primary" style={{cursor:"pointer",backgroundColor:"#B19CD9",border:"#B19CD9"}}>{checkStatus(user)}</Button>
-                            </div>
-                            }
-                        
-                        />
-                    </List.Item>
-                    )}
-                />
+            <div className="col-md-4" style={{height:"0px",}}>
+                <div className="card-body" style={{overflowY:"scroll",height:"330px",scrollbarWidth:"none",zIndex:"1",}}>
+                    <List className="sm" style={{backgroundColor:"white",}}
+                        itemLayout="horizontal"
+                        dataSource={results}
+                        renderItem={(user) => (
+                        <List.Item>
+                            <List.Item.Meta
+                            avatar={<Avatar shape="square" size="large" src={haveImage(user)} />}
+                            title={<div className="d-flex justify-content-between py-2">
+                                    <div style={{fontStyle:"italic"}}>{user.name}</div>
+                                    <Button type="primary" style={{cursor:"pointer",backgroundColor:"#B19CD9",border:"#B19CD9"}}>{checkStatus(user)}</Button>
+                                </div>
+                                }
+                            
+                            />
+                        </List.Item>
+                        )}
+                    />
+                </div>
+                
             </div>
             </>
         )
